c5fd7d529ee389e99a7ab29533723a8508999f30
wazsah <m.w.w.sahebali@student.tue.nl>
2017-11-04 16:42:53 +0100

    Rewrote training loop in tf.slim fashion
    Forward pass not working, because cropping within the model is still required

--- training_loop.py
+++ training_loop.py
@@ -1,51 +1,40 @@
-# Training loop
+import tensorflow as tf
+from network import net
+import json
 from data_load_utils import *
-import time 
-batch_size = 10 # the CamVid training set only contains 367 samples 
-max_epochs = 10
-current_epoch=0
-# Keep track of losses and accuracies
-validation_loss, validation_accuracy = [], []
-train_loss, train_accuracy = [], []
-test_loss, test_accuracy = [], []
-_train_loss, _train_accuracy = [], []
+import numpy as np
 
-# Load validation data
-vlocx, vlocy = parsepaths(valid_paths)
-x_validation, y_validation = loadimages(vlocx, vlocy)
-           
+batch_size, height, width, nchannels = 10, 360, 480, 3
+learning_rate=0.005
+
+locx, locy = parsepaths(train_paths)
+x_im, y_im = loadimages(locx, locy)
+
+with open('model_parameters.json') as params:
+    params_dict = json.load(params)
+
+params_dict['num_features'] = 16
+
+# Forward pass
+# with tf.Session() as sess:
+    # read_input, output = tensorreshape(x_im[0],y_im[0], batch_size, width, height, nchannels)
+    # model=net(read_input, params_dict)
+# Not working, because cropping within the model is still required to solve: 
+#    ValueError: Dimension 2 in both shapes must be equal, but are 45 and 44 for 'skip2_up' (op: 'ConcatV2') with input shapes: [1,60,45,176], [1,60,44,16], [] and with computed input tensors: input[2] = <-1>.
+
+# Training loop
 with tf.Session() as sess:
-    sess.run(tf.global_variables_initializer())
-    print('Start')
-    starttime=time.time()
-    try: 
-        while current_epoch<max_epochs:
-            # Intialize empty looplists
-            # Next batch
-            x_batch, y_batch = nextbatch(x_im, y_im, current_epoch,batch_size)
-            # What to fetch and what to feed
-            fetches_train = [train_op, cross_entropy, accuracy]
-            feed_dict_train = {x_pl: x_batch, y_pl: y_batch}
-            # Feed and fetch
-            _, _loss, _acc = sess.run(fetches_train, feed_dict_train)
-        
-            _train_loss.append(_loss)
-            _train_accuracy.append(_acc)
-            
-            current_epoch+=1
-            
-            # Validation
-            if(current_epoch%10==0):
-                train_loss.append(np.mean(_train_loss))
-                train_accuracy.append(np.mean(_train_accuracy))
-                fetches_validation = [train_op, cross_entropy, accuracy]
-                feed_dict_validation = {x_pl: x_validation, y_pl: y_validation}
-                _loss, _acc = sess.run(fetches_validation, feed_dict_validation)
-                validation_loss.append(_loss)
-                validation_accuracy.append(_acc)
-                _train_loss, _train_accuracy = [], []
-                print("Epoch: %s \n  Train Loss %s, Train acc %s, \nValidation loss %s,  Validation acc %s"%(current_epoch, train_loss[-1], train_accuracy[-1], valid_loss[-1], valid_accuracy[-1]))           
-    except KeyboardInterrupt:
-        pass 
-
-print("Done in %s seconds"%(time.time()-starttime))
+    log_dir="./"
+    # We load a batch and reshape to tensor
+    xbatch, ybatch = nextbatch(x_im, y_im, batchnumber=0, batchsize=batch_size)
+    xb, yb = tensorreshape(xbatch, ybatch, batch_size, width, height, nchannels)
+    # Obtain the predition
+    y_hat, _ = net(xb, params_dict)
+    # We calculate the loss
+    loss = tf.losses.mean_squared_error(labels=targets, predictions=predictions)
+    total_loss = slim.losses.get_total_loss()
+    optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate)
+    train_op = slim.learning.create_train_op(total_loss, optimizer) 
+    final_loss= slim.learning.train(train_op, logdir=log_dir, number_of_steps=200, save_summaries_secs=10, log_every_n_steps=50)
+    
+print("Done. With final loss: %s"%final_loss)
