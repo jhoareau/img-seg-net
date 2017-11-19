9368c6af04bb948c27ef9da54186499ec72a525a
Julien Hoareau <julien.hoareau@outlook.com>
2017-11-19 14:56:54 +0100

    Adding smaller model

--- training_loop.py
+++ training_loop.py
@@ -1,36 +1,42 @@
+import os
+import numpy as np
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 import tensorflow as tf
 from network import net
 import json
 from data_utils import *
-import numpy as np
 slim = tf.contrib.slim
 # This might increase training time, so set to False if desired
 image_in_tensorboard = True
 batch_size, height, width, nchannels = 3, 360, 480, 3
+# batch_size, height, width, nchannels = 3, 240, 320, 3
+final_resized = 224
+# final_resized = 192
 learning_rate = 0.001
+model_version = 56
 
 with open('model_parameters.json') as params:
-    params_dict = json.load(params)
+    params_dict = json.load(params)[repr(model_version)]
 
 params_dict['input_num_features'] = 48
+# params_dict['input_num_features'] = 16
 params_dict['output_classes'] = 12
-params_dict['num_features'] = 16
 
 # Save training data in tfrec and load it into a slim dataset
 
 tfrec_dump(train_paths, "trainset.tfrec")
 tfsdataset = slim_dataset("trainset.tfrec", 367)
 
-gpu_opts = tf.GPUOptions(per_process_gpu_memory_fraction=0.7)
+gpu_opts = tf.GPUOptions(per_process_gpu_memory_fraction=0.9)
 # Training loop
 with tf.Session(config=tf.ConfigProto(gpu_options=gpu_opts)) as sess:
     log_dir = 'train'
     # We load a batch and reshape to tensor
     xbatch, _, ybatch, _ = batch(
-        tfsdataset, batch_size=3, height=360, width=480, resized=224)
-    input_batch = tf.reshape(xbatch, shape=(batch_size, 224, 224, 3))
-    ground_truth_batch = tf.reshape(ybatch, shape=(batch_size, 224, 224, 1))
-        
+        tfsdataset, batch_size=batch_size, height=height, width=width, resized=final_resized)
+    input_batch = tf.reshape(xbatch, shape=(batch_size, final_resized, final_resized, 3))
+    ground_truth_batch = tf.reshape(ybatch, shape=(batch_size, final_resized, final_resized, 1))
+
     # Obtain the prediction
     predictions = net(input_batch, params_dict)
 
@@ -57,7 +63,8 @@ with tf.Session(config=tf.ConfigProto(gpu_options=gpu_opts)) as sess:
     optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate)
     train_op = slim.learning.create_train_op(
         total_loss, optimizer, summarize_gradients=False)
+    print("Number of trainable parameters", np.sum([np.prod(v.shape) for v in tf.trainable_variables()]))
     final_loss = slim.learning.train(
-        train_op, logdir=log_dir, number_of_steps=200, save_summaries_secs=10, log_every_n_steps=50)
+        train_op, logdir=log_dir, number_of_steps=10000, save_summaries_secs=10, log_every_n_steps=50)
 
 print("Done. With final loss: %s" % final_loss)
