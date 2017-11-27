6a1e543d65d6aba90d68eba124bcc5d36d3d0c80
Julien Hoareau <julien.hoareau@outlook.com>
2017-11-27 15:42:27 +0100

    Eval model

--- eval_model.py
+++ eval_model.py
@@ -7,7 +7,9 @@ import json
 from data_utils import *
 slim = tf.contrib.slim
 
-batch_size, height, width, nchannels = 1, 360, 480, 3
+n_images = 101
+
+batch_size, height, width, nchannels = n_images, 360, 480, 3
 final_resized = 224
 model_version = 56
 
@@ -18,7 +20,7 @@ params_dict['input_num_features'] = 48
 params_dict['output_classes'] = 12
 
 tfrec_dump(valid_paths, "validset.tfrec")
-tfsdataset = slim_dataset("validset.tfrec", 101)
+tfsdataset = slim_dataset("validset.tfrec", n_images)
 
 gpu_opts = tf.GPUOptions(per_process_gpu_memory_fraction=0.9)
 # Training loop
@@ -36,27 +38,28 @@ with tf.Session(config=tf.ConfigProto(gpu_options=gpu_opts)) as sess:
     predimmax = tf.expand_dims(
         tf.cast(tf.argmax(predim, axis=3), tf.float32), -1)
 
-    yb = tf.cast(tf.divide(ybatch[0], 11), tf.float32)
-    tf.summary.image("x", xbatch[0], max_outputs=1)
-    tf.summary.image("y", yb, max_outputs=1)
+    yb = tf.cast(tf.divide(ground_truth_batch, 11), tf.float32)
     predim = tf.nn.softmax(predictions)
     predimmax = tf.expand_dims(
         tf.cast(tf.argmax(predim, axis=3), tf.float32), -1)
     predimmax = tf.divide(tf.cast(predimmax, tf.float32), 11)
-    tf.summary.image("y_hat", predimmax, max_outputs=1)
+
+    tf.summary.image("x", input_batch, max_outputs=n_images)
+    tf.summary.image("y", yb, max_outputs=n_images)
+    tf.summary.image("y_hat", predimmax, max_outputs=n_images)
 
     # We calculate the loss
     one_hot_labels = slim.one_hot_encoding(
         tf.squeeze(ground_truth_batch),
         params_dict['output_classes'])
     slim.losses.softmax_cross_entropy(
-        tf.squeeze(predictions),
+        predictions,
         one_hot_labels)
     total_loss = slim.losses.get_total_loss()
     tf.summary.scalar('loss', total_loss)
 
     slim.evaluation.evaluate_once(
         '',
-        'train_aws',
+        'train_aws/model.ckpt-934',
         'val'
     )
