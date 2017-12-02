c06f34800896369dbe43e27adb2e6c41de7a706a
Julien Hoareau <julien.hoareau@outlook.com>
2017-12-02 15:10:20 +0100

    AWS + EBS

--- aws_script.sh
+++ aws_script.sh
@@ -4,3 +4,5 @@ cd ..
 git clone https://github.com/alexgkendall/SegNet-Tutorial.git
 mv SegNet-Tutorial/CamVid CamVid
 cd img-seg-net
+mkdir /dev/xvdba/trainingdata
+ln -s /dev/xvdba/trainingdata train
