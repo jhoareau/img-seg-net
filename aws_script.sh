01f24f67376eaa3ee63e805ec38dce053708e487
Julien Hoareau <julien.hoareau@outlook.com>
2017-12-12 22:50:26 +0100

    Fix AWS script from expecting EBS volume

--- aws_script.sh
+++ aws_script.sh
@@ -4,5 +4,3 @@ cd ..
 git clone https://github.com/alexgkendall/SegNet-Tutorial.git
 mv SegNet-Tutorial/CamVid CamVid
 cd img-seg-net
-mkdir /dev/xvdba/trainingdata
-ln -s /dev/xvdba/trainingdata train
